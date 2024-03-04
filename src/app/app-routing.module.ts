89dfcd782902f7a5c86b7d9de1d7013c818b5c66
niickoh <ni.catalmir@gmail.com>
2024-03-04 20:20:49 -0300

    otro comentario 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)},  
   {path: '', redirectTo:'home',pathMatch:'full'},
 ];
-//borrando el comentario desde develoop
+//borrando el comentario desde develoopjhgjhgjhg
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
