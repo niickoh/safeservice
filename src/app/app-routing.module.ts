8365b6dd226f5d0e88d38cf5a4e29127f264c244
niickoh <ni.catalmir@gmail.com>
2024-03-08 13:44:14 -0300

    probando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)},  
   {path: '', redirectTo:'home',pathMatch:'full'},
 ];
-//borrando  asdasdasd
+//borrando  asdasdasdASas
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
